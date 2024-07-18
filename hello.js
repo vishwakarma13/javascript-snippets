f9c5afb66050a94a78fd8c6121fb675436760a14
Vaibhav Vishwakarma <vaibhav.vishwa1309@gmail.com>
2024-07-18 22:46:20 +0530

    basic string output code with newline

--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
 console.log("Hello")
 console.log("World")
-//single line commment
+//single line commment 
 /*multi line comment in a javasript. This is a basic program which prints two strings i.e. Hello and World*/
