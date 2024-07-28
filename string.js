bd2f9b6b4273a4246ddfee0338d441656caddd21
Vaibhav Vishwakarma <vaibhav.vishwa1309@gmail.com>
2024-07-28 23:50:39 +0530

    create file about different ways to use strings in js

---
+++ string.js
@@ -0,0 +1,14 @@
+// basic string output
+console.log("this is a string")
+
+// to declare a string and its output
+let str = new String("dinosaur")
+console.log(str)
+
+// concatenation of strings
+a = "car"
+b = " is a"
+c = " non-living object"
+console.log(a+b+c)
+
+// using backticks
