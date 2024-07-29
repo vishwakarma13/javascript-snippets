18d7c31c69f307f348f56a0a44036df8e40e9a8f
Vaibhav Vishwakarma <vaibhav.vishwa1309@gmail.com>
2024-07-29 13:57:49 +0530

    defined an array, printed the contets using index

--- array.js
+++ array.js
@@ -1,2 +1,18 @@
-companies = ["microsoft", "nokia", "apple", "samsung"];
+// program to print an array of data
+abc = ["cat", "rat", "bat",];
+console.log(abc);
+
+
+// defining an array
+let companies = new Array("microsoft", "nokia", "apple", "samsung");
 console.log(companies);
+
+
+//to print using index
+console.log(companies[0]);
+console.log(companies[1]);
+console.log(companies[2]);
+console.log(companies[3]);
+console.log(abc[0]);
+console.log(abc[1]);
+console.log(abc[2]);
