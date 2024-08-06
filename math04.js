cdb7e949944928497174f0dc4638d1342719f66a
Vaibhav Vishwakarma <vaibhav.vishwa1309@gmail.com>
2024-08-07 00:15:27 +0530

    ceil and floor program

---
+++ math04.js
@@ -0,0 +1,5 @@
+/* Program using math.ceil and math.floor */
+let num1=34.3;
+console.log("The ceiling value of "+num1+" is "+Math.ceil(num1));
+console.log("The floor value of "+num1+" is "+Math.floor(num1));
+// do not confuse ceil an floor for round off , they are not.
