f58c20a0efec2bbdcc4e951e814bba3bb5669c05
Vaibhav Vishwakarma <vaibhav.vishwa1309@gmail.com>
2024-08-07 00:01:47 +0530

    multiplication and division

---
+++ math03.js
@@ -0,0 +1,9 @@
+/* Division and multiplication */
+let a=33;
+let b=11;
+prod=a*b;
+div=a/b;
+console.log("The product of "+a+" and "+b+" is "+ prod
+);
+console.log("The division of "+a+" and "+b+" is "+ div
+);
