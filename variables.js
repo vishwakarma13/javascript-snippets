093c6bbd406327f55936b1b81bbb692331df8be4
Vaibhav Vishwakarma <vaibhav.vishwa1309@gmail.com>
2024-08-07 13:59:39 +0530

    variable declaration and assigning using const let and var

---
+++ variables.js
@@ -0,0 +1,20 @@
+/* Program to declare variables */
+let var1="A man";
+let var2=" with a fan.";
+let var3=var1 + var2;
+console.log(var3);
+
+// we can also declare variables in a single line
+let var4, var5, var6;
+var4="I";
+var5=" have a car.";
+var6=var4 + var5;
+console.log(var6);
+
+// we can also use variables without declaring them(not recommended)
+var7="This is";
+var8=" a computer.";
+var9=var7 + var8;
+console.log(var9);
+
+/* we can also use const and var to declare variables but var isn't use in modern code since it is global scoped */
