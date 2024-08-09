565c23411f8292d13e6a6163521454a3f6251a72
Vaibhav Vishwakarma <vaibhav.vishwa1309@gmail.com>
2024-08-09 23:47:44 +0530

    added backticks to write strings with variable

--- string.js
+++ string.js
@@ -1,4 +1,4 @@
-// basic string output
+// basic string output , without declaring the string
 console.log("this is a string")
 
 // to declare a string and its output
@@ -12,3 +12,5 @@ c = " non-living object"
 console.log(a+b+c)
 
 // using backticks
+console.log(`This is not a ${a} neither a ${str}.`)
+console.log(`Asteroid is a ${c} and one of it killed ${str}.`)
