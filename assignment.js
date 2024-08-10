058a1a0526c0fe6e0087b005a4ac0cc8dc5a287c
Vaibhav Vishwakarma <vaibhav.vishwa1309@gmail.com>
2024-08-10 22:03:06 +0530

    added basic and addition assignment operator

---
+++ assignment.js
@@ -0,0 +1,13 @@
+/* basic assignment operator */
+{
+    let a = 32; //declaring a and assigning it 32
+    let b = a;  //declaring b and assigning it a
+    console.log(b);
+}
+
+/* addition assigment operator */
+{
+    let a = 10;
+        a += 102;
+    console.log(a);
+}
