61eb197b22be5615dfad4b1723a7d1f5f7562462
Vaibhav Vishwakarma <vaibhav.vishwa1309@gmail.com>
2024-08-11 22:13:51 +0530

    used += for string concatenation

--- assignment.js
+++ assignment.js
@@ -11,3 +11,13 @@
         a += 102;
     console.log(a);
 }
+
+//The same is true for subtraction, multiplication and division
+
+
+//You can also use += for string concatenation
+{
+    let myStr1 = "The sky is ";
+        myStr1 += "full of stars." 
+    console.log(myStr1);
+}
